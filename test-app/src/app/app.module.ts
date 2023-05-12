f645d8449a0a2b248b5063684ba87608c1b53169
pg <grigolopi@gmail.com>
2023-05-12 10:14:34 +0200

    meteo service

--- test-app/src/app/app.module.ts
+++ test-app/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-import { HttpClient } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
@@ -13,7 +13,7 @@ import { ListService } from './list.service';
   imports: [BrowserModule, 
     IonicModule.forRoot(), 
     AppRoutingModule,
-    HttpClient
+    HttpClientModule,
   ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, ListService],
   bootstrap: [AppComponent],
--- test-app/src/app/meteo.service.ts
+++ test-app/src/app/meteo.service.ts
@@ -8,5 +8,8 @@ import { Observable } from 'rxjs';
 })
 export class MeteoService {
 
-  constructor(private) { }
+  constructor(private http: HttpClient) { }
+
+  
+
 }
