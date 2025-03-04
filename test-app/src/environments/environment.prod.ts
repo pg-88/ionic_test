07c6cbbd64f75099fcac0471f41dea1c1af237d0
Pietro <59828604+pg-88@users.noreply.github.com>
2025-03-04 14:47:47 +0100

    Delete test-app directory
    Salvo solo gli appunti

--- test-app/src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  apiKey:'',
-  datiMeteo: 'https://api.openweathermap.org/data/2.5/weather?',
-  previsioniMeteo: 'https://api.openweathermap.org/data/2.5/forecast/?'
-};
